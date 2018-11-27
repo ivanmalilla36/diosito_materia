2672113aecd9f6fa87553e850918aba98b6aea8c
ivanmalilla36 <ivanmalilla36@gmail.com>
2018-11-27 16:15:14 -0700

    se agrego una funcion y la ruta para publicar articulos

--- controllers/articulo.js
+++ controllers/articulo.js
@@ -16,6 +16,29 @@ function getArticulos(req,res){
   });
 }
 
+function postArticulos(req,res){
+  var articulo = new Articulo();
+  var params = req.body;
+  
+  articulo.Nombre = params.Nombre;
+  articulo.Correo = params.Correo;
+  articulo.Titulo = params.Titulo;
+  articulo.Descripcion = params.Descripcion;
+  articulo.Fecha = params.Fecha;
+  articulo.strAmat = params.strAmat;
+  articulo.Imagen = params.Imagen;
+  articulo.Comentarios = [];
+
+  articulo.save((err,articuloSaved)=>{
+    if(err){
+      return res.status(500).send();  
+    }else{
+      return res.status(200).send();
+    }
+  });
+}
+
 module.exports = {
-	getArticulos
+	getArticulos,
+  postArticulos
 }
--- routes/articulo.js
+++ routes/articulo.js
@@ -6,5 +6,6 @@ const api = express.Router()
 
 api.get('/articulos', ArticuloController.getArticulos)
 
+api.post('/postArticulos', ArticuloController.postArticulos)
 
 module.exports = api;
